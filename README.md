8ffcf4bfffc51fac9db3c7c0905beb1a5b7df840
Jez Williams <Jez@jezfx.com>
2017-02-07 00:01:53 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+#Boost 🚀
+
+A webpack boilerplate to get up and running on projects. It includes the following libs:
+
+* jQuery
+* waypoints
+* jquery.scrollto
+* animate.css
+* basic custom styling
+
+in order to quickly build up a production-ready marketing website.
+
+
+###Install dependencies
+
+```
+npm install
+```
+
+###Developing locally
+
+1. Run
+
+	```
+	npm run dev
+	```
+
+2. In your browser, navigate to: [http://localhost:8080/](http://localhost:8080/)
+
+###Publish
+
+1. In `webpack.config.js` file, replace `www.example.com` with the real domain name.
+
+2. Run
+
+	```
+	npm run build
+	```
+
+3. Copy the following folders/files to web server
+
+	/build,
+	/image,
+	index.html
---
+++ js/app.js
@@ -0,0 +1,31 @@
+import '../css/app.scss';
+import $ from "jquery";
+import 'waypoints';
+import 'scrollTo';
+
+/**
+ * EXAMPLE JS STARTS
+ */
+$(function() {
+    // $('[id^=scrollTo]').click(function() {
+    //     var id = $(this).attr('id').slice(9);
+    //     $(window).scrollTo($('#' + id), 1000, { offset: { top: -51, left: 0 } });
+    // });
+
+    // $('#marketing').waypoint(function() {
+    //     $('.img-circle').addClass('animated zoomIn');
+    // }, {
+    //     offset: '50%',
+    //     triggerOnce: true
+    // });
+
+    // $('.featurette').waypoint(function() {
+    //     $('#' + this.element.id + ' .featurette-image').addClass('animated pulse');
+    // }, {
+    //     offset: '50%',
+    //     triggerOnce: true
+    // });
+});
+/**
+ * EXAMPLE JS ENDS
+ */
